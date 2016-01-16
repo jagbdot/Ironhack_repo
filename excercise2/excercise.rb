ca5c3601d642d184fc520267492fd23048d746da
Jagb <Jagb.dot@gmail.com>
2016-01-16 19:37:27 +0100

    excercise2

---
+++ excercise2/excercise.rb
@@ -0,0 +1,7 @@
+def sentence (order)
+	print (((order).split).sort)
+end
+
+sentence "Have a nice day".downcase
+sentence "Fools fall for foolish follies".downcase
+sentence "Ruby is a fun language".downcase
