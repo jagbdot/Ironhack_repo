d160334e1daf5172fb80e47e50e315f974ed0ec4
Jagb <Jagb.dot@gmail.com>
2016-01-18 11:22:03 +0100

    stacked in .ord

--- caesar-cipher/excercise.rb
+++ caesar-cipher/excercise.rb
@@ -1,28 +1,7 @@
-def caesar_cipher(string, shifter)
-    ciphered = []
-    string.split("").each do |char|
-        if char == " " then ciphered << char 
-            next 
-        end
-
-        char = char.to_s.ord
-
-        if ("a".ord.."z".ord).include? char
-            if char + shifter < "z".ord
-                ciphered.push((char + shifter).chr)
-            else
-                ciphered.push((shifter - ("z".ord - char) + "a".ord).chr)
-            end         
-        elsif ("A".ord.."Z".ord).include? char
-            if char + shifter < "Z".ord
-                ciphered.push((char + shifter).chr)
-            else
-                ciphered.push((shifter - ("Z".ord - char) + "A".ord).chr)
-            end 
-        else    
-            next
-        end
-
-    end
-    print ciphered.join + "\n"
+def solve_cipher(input)
+  input.split("").each do |i|
+    print "#{i}".ord, " "
+  end
 end
+
+solve_cipher("ifmmp")
