65500440d2b7836dce071c55706724cb0b8526a1
Jagb <Jagb.dot@gmail.com>
2016-03-07 16:32:33 +0100

    pp completed with mario

---
+++ Week_6/sandwichr_morning_application/app/models/sandwich.rb
@@ -0,0 +1,15 @@
+class Sandwich < ActiveRecord::Base
+  has_many :sandwich_ingredients
+  has_many :ingredients, through: :sandwich_ingredients
+
+  def as_json(options={})
+    super(only: [:id, :name, :bread_type, :total_calories],
+      methods: [:ingredients_count],
+      include: [ingredients: {only: [:id, :name, :calories]}])
+  end
+
+  def ingredients_count
+    self.ingredients.count
+  end
+
+end
